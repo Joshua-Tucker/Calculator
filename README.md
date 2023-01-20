9bae3c9cd78e60c0acd201157e22b2c63838e4a6
Josh Tucker <joshuaatucker@hotmail.com>
2023-01-20 14:28:11 +0000

    operators work

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 
 Okay project 2
 
+BREIF
+A deployed website (using GitHub Pages)
+2.  A public GitHub repository for your codebase
+1.  A README.md with a short intro to the project
+2.  At least 15 Git commits for the different stages in your development
+3.  Be responsive and built mobile-first, it should work on different screen widths
+4.  Accept a minimum of 2 inputs, perform an operation and show the output
+5.  DOES NOT USE THE eval() method (as this is extremely dangerous) TICK
+6.  Code is highly readable (good naming and indented correctly
+
 PLAN
 
 -Go for basic iphone style calculator
@@ -33,12 +43,17 @@ TUES PM
 
 
 QUESTIONS
+How to create function to do mathematical process of the button its presssing
+How to write it so its not limited to num1 + num2 = x and can include multiple numbers and multiple operators
+
+
+
 
 NOTES 
 
 Okay im going to think out loud with my current understanding of using forEach and how we can use that to interact with the operators. Once i think i have an idea of how to do something (or even if I fall flat) then im going to research online on code to use because im not confident with tthe layout process just yet and i feel like that is where im going to get bugs because i dont know how to use them appropriatley yet.
 
-i know havew the numbers showing up in the display, i guess the first thing to do would be to declare the rest of the symbols so i can then play around with them.
+i know havew the numbers showing up in the display, i guess the first thing to do would be to declare the rest of the symbols so i can then play around with them. TICK 
 then i guess the logic would be ...
 if (number) + (operator) + (number) + (equals) has been selected
 then show on display 
@@ -47,6 +62,25 @@ in order to get answer ...
 answer = (number value) + (operator value) + (number value) 
 
 
+I would need to then apply each operator with the mathematical sum that java recognises that relates to the button im pressing
+
+
+what im going to do after lunch is go over the breif again and see what ive done and what else needs doing.
+then im going to write code for the percentage and =/- operator
+from there and ive got a working calculator i want to stop relying so heavily on robs handholding and get used to writing the code myself. the rest of friday would be to take the code ive written, look over it and talk it out so i understand what its doing and how to write each statment then remove code (stored in a safe place obviously) and do the whole thing over again. 
+
+I wont learn until i start doing it myself and making those internal connections by trial and error. I know it, its there to refer back to ONLY IF I CANNOT SOLVE IT, so its time to practise and get those drills in.
+
 OBSERVATIONS
+
+THINGS TO DO
+-go over breif
+-go over code
+-get the last 2 symbols working
+-ensure its responsive
+-ensure text stays in display box and doesnt change overall design
+-Highlight operator to clarify youve pressed on them
+-change scss so it looks slick
+-refector work if i can
 
 SUMMARY
--- main.js
+++ main.js
@@ -1,42 +1,61 @@
-
-
-
 //So from my understanding I have selected all the numbers and the display div with these first two
-const numbers   = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operation");
-const symbols    = document.querySelectorAll(".symbol");
-
-const display   = document.querySelector(".display__container");
-const clear     = document.querySelector(".symbol__clear");
+const   numbers     = document.querySelectorAll(".number");
+const   operators   = document.querySelectorAll(".operation");
+const   symbols     = document.querySelectorAll(".symbol");
+const   display     = document.querySelector(".display__container");
+const   clear       = document.querySelector(".symbol__clear");
+const   equals      = document.querySelector(".operation__equals")
+let     firstNumber;
+let     secondNumber;
+let     operator ;
 
 //This then says, when you click (on a number) it will take the inner html (which is showing as the same as the number presented) and take it to the display div. The += allowing it to then keep the previous number and add on any new additions to the display
-const handleClick = () => {
-    display.innerHTML+= event.target.innerHTML
-}
-//so with this, what i read it as, for each number you click, the handleclick function will be in play and take that number youve clicked into the display
-
-//These display all buttons onto display
-numbers.forEach((number)=> {
-    number.addEventListener("click", handleClick)
-})
-
-operators.forEach((operator)=>{
-    operator.addEventListener("click", handleClick)
-})
-
-symbols.forEach((symbol)=>{
-    symbol.addEventListener("click", handleClick)
-})
-
-
-
-
 
 const handleClear = () => {
     display.innerText = "";
+  };
+
+const handleClick = (event) => {
+  display.innerHTML += event.target.innerHTML;
+};
+
+const handleOperators = (event) => {
+    firstNumber = display.innerHTML;
+    operator=event.target.innerHTML;
+    display.innerHTML+=event.target.innerHTML;
+    console.log(operator);
+};
+
+const handleEquals = () => {
+    const numberArr = display.innerHTML.split(operator)
+    console.log(numberArr)
+    firstNumber = parseFloat(numberArr[0])
+    secondNumber = parseFloat(numberArr[1])
+    if (operator== "+"){
+        display.innerHTML=firstNumber + secondNumber
+    }else if (operator== "-"){
+        display.innerHTML=firstNumber - secondNumber
+    }else if (operator== "÷"){
+        display.innerHTML=firstNumber / secondNumber
+    }else {
+        display.innerHTML=firstNumber * secondNumber
+    }
 }
-clear.addEventListener("click", handleClear)
 
+//so with this, what i read it as, for each number you click, the handleclick function will be in play and take that number youve clicked into the display
 
+//These display all buttons onto display
+numbers.forEach((number) => {
+  number.addEventListener("click", handleClick);
+});
 
+operators.forEach((operator) => {
+  operator.addEventListener("click", handleOperators);
+});
 
+symbols.forEach((symbol) => {
+  symbol.addEventListener("click", handleClick);
+});
+
+equals.addEventListener("click",handleEquals)
+clear.addEventListener("click", handleClear);
