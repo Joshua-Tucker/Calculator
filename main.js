fcb067d552c8041fd33ac12594ea88585c73c4dd
Josh Tucker <joshuaatucker@hotmail.com>
2023-01-21 18:12:49 +0000

    percentage done

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 const   numbers             = document.querySelectorAll(".number");
 const   currentOperators    = document.querySelectorAll(".operation");
 const   symbols             = document.querySelectorAll(".symbol");
-const   percentage          = document.querySelector(".symbol__percentage")
 const   display             = document.querySelector(".display__container");
 const   clear               = document.querySelector(".symbol__clear");
 const   backSpace           = document.querySelector(".symbol__backspace")
@@ -27,34 +26,6 @@ const handleBackSpace =(event) => {
   console.log(backSpace.innerHTML)
 }
 
-// const handlePercentage = (event)=> {
-//   display.innerHTML += event.target.innerHTML
-//   const percentArr = display.innerHTML.split(percentage)
-//   console.log(percentArr)
-//   firstNumber = parseFloat(percentArr[0])
-//   secondNumber = parseFloat(percentArr[1])
-
-//   display.innerHTML = (100 * percentArr[0]) / percentArr[1];
-//   }
-
-
-const percentageSum = (firstNumber, secondNumber) => {
-  display.innerHTML= ((firstNumber/100)* secondNumber).toFixed(2)
-}
-
-const handlePercentage = (event) =>{
-  if(display.innerHTML += event.target.innerHTML){
-    display.innerHTML += percentageSum
-  }
-
-
-
-
-}
-
-percentage.addEventListener("click",handlePercentage);
-
-
 const handlecurrentOperators = (event) => {
   firstNumber = display.innerHTML;
   currentOperator=event.target.innerHTML;
@@ -68,7 +39,7 @@ const handleEquals = () => {
     console.log(numberArr)
     firstNumber = parseFloat(numberArr[0])
     secondNumber = parseFloat(numberArr[1])
-
+  
     //display.innerHTML = calculate(firstNumber, secondNumber, currentOperator)
 
     //create function
@@ -78,9 +49,10 @@ const handleEquals = () => {
         display.innerHTML=firstNumber - secondNumber
     }else if (currentOperator== "÷"){
         display.innerHTML=firstNumber / secondNumber
-    }else {
-        display.innerHTML=firstNumber * secondNumber
-    }
+    }else if (currentOperator== "x"){
+        display.innerHTML=firstNumber * secondNumber}
+        else {display.innerHTML = ((firstNumber/100)* secondNumber)}
+    
 }
 
 numbers.forEach((number) => {
@@ -90,7 +62,6 @@ numbers.forEach((number) => {
 currentOperators.forEach((method) => {
   method.addEventListener("click", handlecurrentOperators);
 });
-
 
 
 equals.addEventListener("click",handleEquals);
